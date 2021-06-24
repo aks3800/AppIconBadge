c66b8e9487211c64f9361136f66de903e2fd2e45
Akshat Sharma <akshat.sharma@innovaccer.com>
2021-06-24 16:27:59 +0530

    - example project

--- app_icon_badge/example/ios/Runner/AppDelegate.swift
+++ app_icon_badge/example/ios/Runner/AppDelegate.swift
@@ -3,11 +3,31 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        requestPermission()
+        GeneratedPluginRegistrant.register(with: self)
+        UNUserNotificationCenter.current().delegate = self
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func requestPermission() -> Void {
+        let notificationsCenter = UNUserNotificationCenter.current()
+        notificationsCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("error while requesting permission: \(error.localizedDescription)")
+            }
+            if granted {
+                print("permission granted")
+            } else {
+                print("permission denied")
+            }
+        }
+    }
 }
